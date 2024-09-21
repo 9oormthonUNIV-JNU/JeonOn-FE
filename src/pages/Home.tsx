d4638e2c81c0f90949964a9182fee0e6aeeed3a1
조인식 <imsif42@naver.com>
2024-09-21 20:05:25 +0900

    docs: shadcn/ui drawer 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import home from '@/../public/assets/images/home.png';
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-black h-screen">
       <img src={home} alt="home" />
     </div>
   );

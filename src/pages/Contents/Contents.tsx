e991b33bdb6e60063a58384582390f1cfaa94176
조인식 <imsif42@naver.com>
2024-10-12 01:14:27 +0900

    feat: 콘텐츠 스타일링

--- src/pages/Contents/Contents.tsx
+++ src/pages/Contents/Contents.tsx
@@ -1,3 +1,40 @@
+// import favorites from '@/../public/assets/svgs/favorites.svg';
+import bookmark from '@/../public/assets/svgs/bookmark.svg';
+import { useNavigate } from 'react-router-dom';
+
 export default function Contents() {
-  return <div>contents</div>;
+  const navigate = useNavigate();
+
+  const items = [{ id: '1' }, { id: '2' }, { id: '3' }];
+  return (
+    <div className="h-screen overflow-hidden">
+      <h1 className="text-[#0F0] text-[35px] text-center font-bold mb-10">
+        콘텐츠
+      </h1>
+      <div className="flex flex-col justify-center items-center gap-5 px-5">
+        {items.map((item) => (
+          <div
+            id={item.id}
+            className="w-full h-24 bg-white rounded-2xl px-5 py-3"
+            onClick={(e) => navigate(`/contents/${e.currentTarget.id}`)}
+          >
+            <div className="flex justify-between items-start">
+              <h3 className="text-xl">제목</h3>
+              <div>
+                <img src={bookmark} alt="favorites" />
+              </div>
+            </div>
+            <div>
+              <span className="text-xs font-normal">
+                본문내용본문내용 본문내용 본문내용
+              </span>
+            </div>
+            <div className="flex justify-end items-end">
+              <span className="text-[10px]">2024-01-01</span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/pages/Contents/ContentsDetail.tsx
+++ src/pages/Contents/ContentsDetail.tsx
@@ -1,3 +1,34 @@
+import bookmark from '@/../public/assets/svgs/guide/bookmark.svg';
+// import favorites from '@/../public/assets/svgs/guide/favorites.svg';
+import divideLine from '@/../public/images/divideLine.png';
+
+import GuideCarousel from '@/components/guide/GuideCarousel';
+import { useParams } from 'react-router-dom';
+
 export default function ContentsDetail() {
-  return <div>detail page</div>;
+  const { id } = useParams();
+  return (
+    <div className="h-screen overflow-hidden">
+      <h1 className="text-[#0F0] text-[35px] text-center font-bold mb-10">
+        콘텐츠
+      </h1>
+      <div className="px-6">
+        <div className="mb-1 flex justify-between items-center">
+          <h1 className="text-white text-3xl">제목</h1>
+          <div>
+            <img src={bookmark} alt="favorites" />
+          </div>
+        </div>
+        <div className="mb-3">
+          <span className="text-white text-xs">2024-01-01</span>
+        </div>
+
+        <GuideCarousel />
+
+        <div>
+          <img src={divideLine} alt="divide-line" />
+        </div>
+      </div>
+    </div>
+  );
 }

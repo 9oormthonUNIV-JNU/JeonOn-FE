423502da191f115af3f5faa60b0e0ff356b60dbb
LBB <202780@jnu.ac.kr>
2024-10-13 21:40:47 +0900

    Design: feature/#5 merge 반영 후, 반응형 레이아웃 작업 시작 9oormthonUNIV-JNU#13

--- src/pages/Booth.tsx
+++ src/pages/Booth.tsx
@@ -1,3 +1,20 @@
 export default function Booth() {
-  return <div>Booth!</div>;
+  return (
+    <div className="h-screen flex flex-col items-center">
+      <h1 className="text-[#0F0] text-[35px] text-center font-bold mb-14">
+        부스
+      </h1>
+      <div className="flex justify-center items-center gap-[30px] px-[30px] text-center">
+        <div className="text-white text-[50px] font-normal font-['Pretendard']">
+          5
+        </div>
+        <div className="text-white text-[50px] font-normal font-['Pretendard']">
+          6
+        </div>
+        <div className="text-white text-[50px] font-normal font-['Pretendard']">
+          7
+        </div>
+      </div>
+    </div>
+  );
 }

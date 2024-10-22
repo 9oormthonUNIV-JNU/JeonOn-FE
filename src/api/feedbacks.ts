2dc0acdc3787b5b053c0165869411582fbdcf59c
kyungmin <02dlrudals@gmail.com>
2024-10-22 16:42:39 +0900

    feat: 피드백 리스트 및 피드백 상세정보 API 연동

--- src/api/feedbacks.ts
+++ src/api/feedbacks.ts
@@ -5,11 +5,11 @@ type FeedbackIdType = number;
 export async function getFeedbackList(categoryQuery: string = "") {
   const result = await api.get(`admins/feedbacks?category=${categoryQuery}`);
   
-  return result.data.feedbacks;
+  return result.data.data;
 }
 
 export async function getFeedbackDetail(feedbackId: FeedbackIdType) {
   const result = await api.get(`admins/feedbacks/${feedbackId}`);
   
-  return result.data; 
+  return result.data.data; 
 }

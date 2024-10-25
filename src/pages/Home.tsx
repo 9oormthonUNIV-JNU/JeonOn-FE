0d9f858a74708f89e16bf86f036acd6017ef3925
조인식 <imsif42@naver.com>
2024-10-25 23:03:41 +0900

    fix: qa후 수정 1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import home from '@/../public/images/home.png';
 import homeMain from '@/../public/images/mainHome.png';
 import Footer from '@/components/Home/Footer';
 import { useNavigate } from 'react-router-dom';
@@ -11,42 +10,42 @@ export default function Home() {
         <div className="mb-16">
           <img src={homeMain} alt="home" />
         </div>
-        <div className="px-10 flex flex-col justify-center items-center gap-3 pb-5 mb-24">
+        <div className="px-10 flex flex-col justify-center items-center gap-3 pb-5 mb-40">
           <div
-            className="w-full h-14 bg-main-guide rounded-3xl flex flex-col justify-center items-center text-[#0F0] shadow-2xl border border-[#0F0]"
+            className="w-full h-14 bg-main-guide rounded-[20px] flex flex-col justify-center items-center text-[#0F0] shadow-2xl border border-[#0F0]"
             onClick={() => navigate('/guide')}
           >
             <h1 className="text-xl font-cafe24">안내</h1>
-            <span className="text-xs">
+            <span className="text-xs text-[] font-normal">
               지도와 제휴업체를 한 눈에 볼 수 있어요!
             </span>
           </div>
           <div
-            className="w-full h-14 bg-main-guide rounded-3xl flex justify-center items-center text-[#0F0] text-xl shadow-2xl border border-[#0F0] font-cafe24"
+            className="w-full h-14 bg-main-guide rounded-[20px] flex justify-center items-center text-[#0F0] text-xl shadow-2xl border border-[#0F0] font-cafe24"
             onClick={() => navigate('/contents')}
           >
             콘텐츠
           </div>
           <div
-            className="w-full h-14 bg-main-guide rounded-full flex justify-center items-center text-[#0F0] text-xl shadow-2xl border border-[#0F0] font-cafe24"
+            className="w-full h-14 bg-main-guide rounded-[20px] flex justify-center items-center text-[#0F0] text-xl shadow-2xl border border-[#0F0] font-cafe24"
             onClick={() => navigate('/time-table')}
           >
             타임테이블
           </div>
           <div
-            className="w-full h-14 bg-main-guide rounded-full flex justify-center items-center text-[#0F0] text-xl shadow-2xl border border-[#0F0] font-cafe24"
+            className="w-full h-14 bg-main-guide rounded-[20px] flex justify-center items-center text-[#0F0] text-xl shadow-2xl border border-[#0F0] font-cafe24"
             onClick={() => navigate('/booth')}
           >
             부스
           </div>
           <div
-            className="w-full h-14 bg-main-guide rounded-full flex justify-center items-center text-[#0F0] text-xl shadow-2xl border border-[#0F0] font-cafe24"
+            className="w-full h-14 bg-main-guide rounded-[20px] flex justify-center items-center text-[#0F0] text-xl shadow-2xl border border-[#0F0] font-cafe24"
             onClick={() => navigate('/time-capsule')}
           >
             타임캡슐
           </div>
           <div
-            className="w-full h-14 bg-main-guide rounded-full flex justify-center items-center text-[#0F0] text-xl shadow-2xl border border-[#0F0] font-cafe24"
+            className="w-full h-14 bg-main-guide rounded-[20px] flex justify-center items-center text-[#0F0] text-xl shadow-2xl border border-[#0F0] font-cafe24"
             onClick={() => navigate('/feedback')}
           >
             피드백

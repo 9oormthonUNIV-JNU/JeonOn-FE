c17bf8998d2a9581d0269d8aa46f8acae6bdc6bc
조인식 <imsif42@naver.com>
2024-11-05 05:30:23 +0900

    fix: 타임 수정

--- src/utils/dateStr.ts
+++ src/utils/dateStr.ts
@@ -4,10 +4,10 @@ export function formatDateToYYYYMMDD(dateStr: string) {
 
   const formattedDate =
     date.getFullYear() +
-    "-" +
-    String(date.getMonth() + 1).padStart(2, "0") +
-    "-" +
-    String(date.getDate()).padStart(2, "0");
+    '-' +
+    String(date.getMonth() + 1).padStart(2, '0') +
+    '-' +
+    String(date.getDate()).padStart(2, '0');
 
   return formattedDate;
 }
@@ -15,23 +15,19 @@ export function formatDateToYYYYMMDD(dateStr: string) {
 export function formatDateToMMDDhhmm(dateStr: string) {
   const date = new Date(dateStr);
 
-  const kstOffset = 9 * 60;
+  const kstOffset = 9 * 60; // KST (UTC+9) offset in minutes
   const utcOffset = date.getTimezoneOffset();
   const kstDate = new Date(date.getTime() + (kstOffset - utcOffset) * 60000);
 
-  const utcDate = new Date(kstDate.getTime() - 9 * 60 * 60000);
-
   const formattedDate =
-    String(utcDate.getMonth() + 1).padStart(2, "0") +
-    "-" +
-    String(utcDate.getDate()).padStart(2, "0");
+    String(kstDate.getMonth() + 1).padStart(2, '0') +
+    '-' +
+    String(kstDate.getDate()).padStart(2, '0');
 
   const formattedTime =
-    String(utcDate.getHours()).padStart(2, "0") +
-    ":" +
-    String(utcDate.getMinutes()).padStart(2, "0");
+    String(kstDate.getHours()).padStart(2, '0') +
+    ':' +
+    String(kstDate.getMinutes()).padStart(2, '0');
 
   return `${formattedDate} ${formattedTime}`;
-
 }
-
